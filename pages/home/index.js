914756b750ef48a5e81781ccc936f292bc9a3bd5
Laisa <laisacristina2606@gmail.com>
2022-12-16 22:20:54 -0300

    Salvando

--- pages/home/index.js
+++ pages/home/index.js
@@ -6,8 +6,7 @@ function renderVagas(vagas)
     vagas.forEach(vaga => {
         const posts = criarVagas(vaga)
         listas.append(posts) 
-    });
-
+    }); 
     localStorage.setItem("@kenzieWebwoman", JSON.stringify(jobsData))
 }
 
@@ -35,7 +34,21 @@ function criarVagas(vaga)
     spanTipo.classList.add('tipo__vaga')
 
     const button = document.createElement('button')
-    button.classList.add('button__candidatar')
+    // button.classList.add('button__candidatar')
+
+    if(vagasAlreadyExists(vaga) >= 0){
+        button.classList.add('button__candidatar', 'button__fav')
+        button.innerText = 'Remover'
+        button.dataset.id = vaga.id
+    }else{
+        button.classList.add('button__candidatar')
+        button.innerText = 'Candidatar'
+        button.dataset.id = vaga.id
+    }
+
+    button.addEventListener('click', () => {
+        selecaoRemove(vaga, button)
+    })
 
     h2.innerText = vaga.title
 
@@ -47,8 +60,8 @@ function criarVagas(vaga)
 
     spanTipo.innerText = vaga.modalities[0]
     
-    button.innerText = 'Candidatar'
-    button.dataset.id = vaga.id
+    // button.innerText = 'Candidatar'
+    // button.dataset.id = vaga.id
 
     divButton.append(spanTipo, button)
     div.append(h2, pEmpresa, pCidade, descricao, divButton)
@@ -59,10 +72,11 @@ function criarVagas(vaga)
 function renderCart(array)
 {
     const cartList = document.querySelector('.container__aside')
+    const cardAdds = JSON.parse(localStorage.getItem("@kenzieWebwoman"))
 
     cartList.innerHTML = ''
 
-    if(vagaDoAside.length <= 0){
+    if(cardAdds.length <= 0){
         const descAsides = descAside()
 
         cartList.appendChild(descAsides)
@@ -70,6 +84,7 @@ function renderCart(array)
         array.forEach(vaga => {
             const selecao = createCartProduct(vaga)
             cartList.appendChild(selecao)
+            
         })
     }
 }
@@ -122,36 +137,81 @@ function createCartProduct(vaga)
     return liAside
 }
 
+function getFavoriteArray()
+{
+    return JSON.parse(localStorage.getItem("@kenzieWebwoman:vagas")) || []
+}
+
+function vagasAlreadyExists(vagaSearch)
+{
+    const vagaSelecionada = getFavoriteArray()
+
+    return vagaSelecionada.findIndex(vaga => vaga.id === vagaSearch.id)
+}
+
+function selecaoRemove(vaga, button)
+{
+    const vagaExistente = vagasAlreadyExists(vaga)
+    let vagaSelecionada = getFavoriteArray()
+
+    if(vagaExistente < 0){
+        vagaSelecionada.push(vaga)
+        button.innerText = 'Remover'
+        button.classList.add('button__fav')
+    }else{
+        vagaSelecionada.splice(vagaExistente, 1)
+        button.innerText = 'Candidatar'
+        button.classList.remove('button__fav')
+    }
+    localStorage.setItem("@kenzieWebwoman:vagas", JSON.stringify(vagaSelecionada))
+}
+
 function addVaga()
 {
     const buttons = document.querySelectorAll('.button__candidatar')
-    const cardAdd = JSON.parse(localStorage.getItem("@kenzieWebwoman:vagas")) || []
-    const cardAdds = JSON.parse(localStorage.getItem("@kenzieWebwoman"))
-
+//     const cardAdd = JSON.parse(localStorage.getItem("@kenzieWebwoman:vagas")) || []
+//     const cardAdds = JSON.parse(localStorage.getItem("@kenzieWebwoman"))
+    
     buttons.forEach(button => {
+//         button.addEventListener('click', (event) => {
+//             const vagass = cardAdds.find(vaga => vaga.id === Number(event.target.dataset.id))          
+          
+//             const vagasAside = {
+//                 ...vagass, 
+//                 vagaId: cardAdd.length + 1
+//             }
+//             cardAdd.push(vagasAside)
+//             localStorage.setItem("@kenzieWebwoman:vagas", JSON.stringify(cardAdd))
+//             if(cardAdd.includes(vagasAside)){
+//                 button.classList.add('button__fav')
+//                 button.innerText = 'Remover candidatura'
+//             }
+//         })    
+//         renderCart(cardAdd)
+        
+
         button.addEventListener('click', (event) => {
-            const vagass = cardAdds.find(vaga => {
+            const vagass = jobsData.find(vaga => {
                 return vaga.id === Number(event.target.dataset.id)
             })   
             const vagasAside = {
                ...vagass, 
-               vagaId: cardAdd.length + 1
+               vagaId: vagaDoAside.length + 1
             }
-            cardAdd.push(vagasAside)
-            localStorage.setItem("@kenzieWebwoman:vagas", JSON.stringify(cardAdd))
-            renderCart(vagasAside)
+            vagaDoAside.push(vagasAside)
+            renderCart(vagaDoAside)
         })
     })
 }
 
 function removeVaga(item)
 {
-   vagaDoAside = vagaDoAside.filter((item1) => {
+    vagaDoAside = vagaDoAside.filter((item1) => {
         if(item1 !== item){
             return item1
         }
-   })
-   renderCart(vagaDoAside)
+    })
+    renderCart(vagaDoAside)
 }
 
 renderVagas(jobsData)
